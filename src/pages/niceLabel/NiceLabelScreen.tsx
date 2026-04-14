9b2bc9d5b42259c8df2442d4003fc4e02bb3f82f
Flor Stellamans <florstellamamans@gmail.com>
2026-04-14 10:04:47 +0200

    NiceLabelScreen: Create files

---
+++ src/pages/niceLabel/NiceLabelScreen.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import styles from "./NiceLabelScreen.module.css"
+
+export default function NiceLabelScreen() {
+    
+
+    return (
+        <div>
+            <h1>NiceLabel screen</h1>
+        </div>
+    )
+}
