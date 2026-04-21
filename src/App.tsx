1e144fce9acb633b8bc921de115d7a9e0dede514
Flor Stellamans <florstellamamans@gmail.com>
2026-04-21 09:25:59 +0200

    App.tsx structure update

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,26 @@ import NiceLabelScreen from './pages/NiceLabelScreen'
 
 export default function App() {
   return (
-    <div>
-      <h1>Altec Tools</h1>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomeScreen />}/>
-          <Route path='/labels' element={<LabelScreen />} />
-          <Route path='/printers' element={<PrinterScreen />} />
-          <Route path='/nicelabel' element={<NiceLabelScreen />} />
-        </Routes>
+          <div className="min-h-screen bg-altec-white">
+              <header className="bg-altec-gray px-8 py-4 shadow-md flex items-center justify-between">
+                  <div>
+                      <h1 className="text-black text-4xl font-bold tracking-wide">
+                          Altec tools
+                      </h1>
+                  </div>
+                  <Navbar />
+                  <img src="/src/assets/logo.png" alt="logo" className="h-15 w-auto"/>
+              </header>
+              <main>
+                  <Routes>
+                      <Route path="/" element={<HomeScreen />}/>
+                      <Route path='/labels' element={<LabelScreen />} />
+                      <Route path='/printers' element={<PrinterScreen />} />
+                      <Route path='/nicelabel' element={<NiceLabelScreen />} />
+                  </Routes>
+              </main>
+            </div>
       </BrowserRouter>
-    </div>
   )
 }
